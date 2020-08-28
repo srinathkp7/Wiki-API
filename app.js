850a8ccd09ab516571fe537a9b70755eae0adf6c
srinathkp7 <srinathkp7@gmail.com>
2020-08-28 19:41:06 +0530

    First Commit

--- app.js
+++ app.js
@@ -122,7 +122,12 @@ app.route("/article/:articleTitle")
   );
 });
 
+let port = process.env.PORT;
+if (port == null || port == "") {
+  port = 3000;
+}
 
-app.listen(3000, function(){
-  console.log("Server started on port 3000");
+
+app.listen(port, function(){
+  console.log("Server started Successfully.");
 });
